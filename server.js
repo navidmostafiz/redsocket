f027bb218295d656a13a9c88853b1fcadf883bc1
navidmostafiz <navidmostafiz@gmail.com>
2018-05-16 00:25:11 -0400

    custom namespace, cleint nicknames

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+
+var users = [];
+var chats = [];
 // var path = require('path');
 
 // app.use(express.static('public'))
@@ -12,10 +15,23 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
-        console.log('chat message', msg);
+var redvarse = io.of('/redvarse');
+
+redvarse.on('connection', function (socket) {
+    socket.on('broadcast', function (msg) {
+        if (msg != '> ' && msg != '' && msg != ' ') {
+            redvarse.emit('broadcast', msg);
+        }
+        // var roster = io.sockets.clients('chatroom1');
+        // for (i in roster) {
+        //     console.log('Username: ' + roster[i]);
+        // }
+    });
+
+    socket.on('send-nickname', function (nickname) {
+        socket.nickname = nickname;
+        users.push(socket.nickname);
+        console.log(users);
     });
 });
 

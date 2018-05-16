52b1744b7e476993604ddc890ad389a553b14ada
Navid Mostafiz <navidmostafiz@gmail.com>
2018-05-15 22:45:20 -0400

    first commit add files

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+// var path = require('path');
+
+// app.use(express.static('public'))
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+        console.log('chat message', msg);
+    });
+});
+
+http.listen(port, function () {
+    console.log('listening on *:' + port);
+});
